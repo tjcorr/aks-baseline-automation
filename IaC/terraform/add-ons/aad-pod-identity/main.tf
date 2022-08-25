b3500325bd1b168413b0de60be9a50d30f4b503e
T.J. Corrigan <tjcorr@github.com>
2022-08-25 15:57:38 +0000

    update to newer version of azurerm provider

--- IaC/terraform/add-ons/aad-pod-identity/main.tf
+++ IaC/terraform/add-ons/aad-pod-identity/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.88.1"
+      version = ">= 3.7.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
